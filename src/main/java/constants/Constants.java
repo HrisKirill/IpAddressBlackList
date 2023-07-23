9381faf8c7d736be4e3df7eab7cf06768dd3f9e2
HrisKirill <hristoforow.kirill@gmail.com>
2023-07-23 19:39:36 +0300

    added constants

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public final class Constants {
+
+    public static final String PROPERTIES = "application.properties";
+
+    public static final String BLACKLIST_PROPERTY = "blacklist.file.name";
+
+    public static final String TEXT_TO_EXIT_PROGRAM = "quit";
+
+}
